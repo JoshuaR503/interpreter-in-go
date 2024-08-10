f505d92f6bfb0522038a32dac2e4f81d022d087f
Joshua Garcia <josshuagarciia000@gmail.com>
2024-08-09 20:40:46 -0700

    Started Read Eval Print Loop.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"interpreter/repl"
+	"os"
 	"os/user"
 )
 
@@ -13,5 +15,5 @@ func main() {
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
-	// repl.Start(os.Stdin, os.Stdout)
+	repl.Start(os.Stdin, os.Stdout)
 }
